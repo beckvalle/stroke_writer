c3193e723f8c61785e98af5923aa47a90dd459fa
Becky <25420142+beckvalle@users.noreply.github.com>
2022-07-29 10:13:19 -0500

    add drawing with start and end

--- main.py
+++ main.py
@@ -6,10 +6,17 @@ from tkinter import ttk
 root = Tk()
 
 class Stroke_canvas(Canvas):
+    elements = []
+    color = 'blue'
+
     def __init__(self, parent, **kwargs):
         super().__init__(parent, **kwargs)
         self.bind("<Button-1>", self.save_posn)
+        self.bind("<Button-1>", self.change_color_to_red, add='+')
+        self.bind("<Button-1>", self.add_circle, add='+')
         self.bind("<B1-Motion>", self.add_line)
+        self.bind("<B1-ButtonRelease>", self.change_color_to_blue)
+        self.bind("<B1-ButtonRelease>", self.add_circle, add='+')
         
     def save_posn(self, event):
         self.lastx, self.lasty = event.x, event.y
@@ -18,17 +25,34 @@ class Stroke_canvas(Canvas):
         self.create_line((self.lastx, self.lasty, event.x, event.y))
         self.save_posn(event)
 
+    def add_circle(self, event):
+        self.create_oval(event.x-5, event.y-5, event.x+5, event.y+5, fill=self.color)
+
+    def change_color_to_red(self, event):
+        self.color = 'red'
+
+    def change_color_to_blue(self, event):
+        self.color = 'blue'
+
     def draw_triangle(self):
         self.create_polygon(10, 10, 200, 50, 10, 50, fill='red', outline='blue')
 
     def draw_text(self):
-        self.create_text(100, 100, text='sample text', 
-        anchor='nw', font='TkMenuFont', fill='black')
+        self.elements.append(self.create_text(100, 100, text='sample text', 
+        anchor='nw', font='TkMenuFont', fill='black'))
+
+    def delete_text(self):
+        del_id = self.elements[0]
+        self.delete(del_id)
+        self.elements.remove(del_id)
+
+    def list_element_ids(self):
+        print([x for x in self.elements])
 
 content = ttk.Frame(root, padding=(3,3,12,12))
 # frame = ttk.Frame(content, borderwidth=5, relief="ridge", width=200, height=100)
 name_lbl = ttk.Label(content, text="Name")
-name = ttk.Entry(content)
+# name_entry = ttk.Entry(content)
 
 show_letter_var = BooleanVar()
 show_stroke_var = BooleanVar()
@@ -51,10 +75,13 @@ sketch = Stroke_canvas(content)
 sketch.grid(column=0, row=0, columnspan=4, rowspan=2, sticky=(N, W, E, S))
 sketch.draw_triangle()
 sketch.draw_text()
+sketch.list_element_ids()
+sketch.delete_text()
+sketch.list_element_ids()
 
 # frame.grid(column=0, row=0, columnspan=4, rowspan=2, sticky=(N, S, E, W))
 name_lbl.grid(column=4, row=0, columnspan=2, sticky=(N, W), padx=5)
-name.grid(column=4, row=1, columnspan=2, sticky=(N,E,W), pady=5, padx=5)
+# name_entry.grid(column=4, row=1, columnspan=2, sticky=(N,E,W), pady=5, padx=5)
 show_letter_bttn.grid(column=0, row=3)
 show_stroke_bttn.grid(column=1, row=3)
 clear_bttn.grid(column=2, row=3)
