f8dc02a2198ec4cd69dcffde0ab6a18076cd5a5d
Becky <25420142+beckvalle@users.noreply.github.com>
2022-07-29 10:26:33 -0500

    added clear canvas function

--- main.py
+++ main.py
@@ -7,39 +7,38 @@ root = Tk()
 
 class Stroke_canvas(Canvas):
     elements = []
-    color = 'blue'
+    point_color = 'blue'
+    stroke_number = 0
 
     def __init__(self, parent, **kwargs):
         super().__init__(parent, **kwargs)
         self.bind("<Button-1>", self.save_posn)
-        self.bind("<Button-1>", self.change_color_to_red, add='+')
+        self.bind("<Button-1>", self.change_pt_color_to_red, add='+')
         self.bind("<Button-1>", self.add_circle, add='+')
         self.bind("<B1-Motion>", self.add_line)
-        self.bind("<B1-ButtonRelease>", self.change_color_to_blue)
+        self.bind("<B1-ButtonRelease>", self.change_pt_color_to_blue)
         self.bind("<B1-ButtonRelease>", self.add_circle, add='+')
+        self.bind("<B1-ButtonRelease>", self.increment_stroke, add='+')
         
     def save_posn(self, event):
         self.lastx, self.lasty = event.x, event.y
 
     def add_line(self, event):
-        self.create_line((self.lastx, self.lasty, event.x, event.y))
+        self.create_line((self.lastx, self.lasty, event.x, event.y), width=4)
         self.save_posn(event)
 
     def add_circle(self, event):
-        self.create_oval(event.x-5, event.y-5, event.x+5, event.y+5, fill=self.color)
+        self.create_oval(event.x-5, event.y-5, event.x+5, event.y+5, 
+            fill=self.point_color)
 
-    def change_color_to_red(self, event):
-        self.color = 'red'
+    def change_pt_color_to_red(self, event):
+        self.point_color = 'red'
 
-    def change_color_to_blue(self, event):
-        self.color = 'blue'
+    def change_pt_color_to_blue(self, event):
+        self.point_color = 'blue'
 
-    def draw_triangle(self):
-        self.create_polygon(10, 10, 200, 50, 10, 50, fill='red', outline='blue')
-
-    def draw_text(self):
-        self.elements.append(self.create_text(100, 100, text='sample text', 
-        anchor='nw', font='TkMenuFont', fill='black'))
+    def increment_stroke(self, event):
+        self.stroke_number += 1
 
     def delete_text(self):
         del_id = self.elements[0]
@@ -49,10 +48,17 @@ class Stroke_canvas(Canvas):
     def list_element_ids(self):
         print([x for x in self.elements])
 
+
 content = ttk.Frame(root, padding=(3,3,12,12))
 # frame = ttk.Frame(content, borderwidth=5, relief="ridge", width=200, height=100)
 name_lbl = ttk.Label(content, text="Name")
 # name_entry = ttk.Entry(content)
+
+sketch = Stroke_canvas(content)
+sketch.grid(column=0, row=0, columnspan=4, rowspan=2, sticky=(N, W, E, S))
+
+def clear_canvas():
+    sketch.delete('all')
 
 show_letter_var = BooleanVar()
 show_stroke_var = BooleanVar()
@@ -65,19 +71,15 @@ show_letter_bttn = ttk.Checkbutton(content, text="Show letter",
 show_stroke_bttn = ttk.Checkbutton(content, text="Show stroke", 
     variable=show_stroke_var, onvalue=True)
 
-clear_bttn = ttk.Button(content, text="Clear")
+clear_bttn = ttk.Button(content, text="Clear", command=clear_canvas)
 ok_bttn = ttk.Button(content, text="Okay")
 cancel_bttn = ttk.Button(content, text="Cancel")
 
 content.grid(column=0, row=0, sticky=(N, S, E, W))
 
-sketch = Stroke_canvas(content)
-sketch.grid(column=0, row=0, columnspan=4, rowspan=2, sticky=(N, W, E, S))
-sketch.draw_triangle()
-sketch.draw_text()
-sketch.list_element_ids()
-sketch.delete_text()
-sketch.list_element_ids()
+
+
+
 
 # frame.grid(column=0, row=0, columnspan=4, rowspan=2, sticky=(N, S, E, W))
 name_lbl.grid(column=4, row=0, columnspan=2, sticky=(N, W), padx=5)
