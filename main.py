3503132d8d864c0aa8757246e74392e55f64453d
Becky <25420142+beckvalle@users.noreply.github.com>
2022-07-29 21:05:40 -0500

    added loading function

--- main.py
+++ main.py
@@ -15,10 +15,12 @@ def close_window(window):
 show_outline_var = BooleanVar()
 show_arrows_var = BooleanVar()
 stroke_number_var = IntVar()
+current_letter_var = StringVar()
 
 show_outline_var.set(True)
 show_arrows_var.set(True)
 stroke_number_var.set(0)
+current_letter_var.set('')
 
 class Stroke_canvas(Canvas):
 
@@ -74,6 +76,7 @@ class Stroke_canvas(Canvas):
     def clear_stroke(self):
         self.stroke_end_id_holder = []
         self.stroke_line_id_holder = []
+        self.coords_holder = []
         stroke_number_var.set(0)
         stroke_num_lbl.config(text="Stroke: ")
 
@@ -97,6 +100,7 @@ class Stroke_canvas(Canvas):
                 writer = csv.DictWriter(csvfile, delimiter=',', fieldnames=self.fieldnames)
                 writer.writerow({"symbol_name":ret_val, "strokes":('-').join(self.coords_holder)})
             close_window(message_window)
+            self.clear_written_strokes()
 
         message_window = Toplevel(root)
         message_window.grab_set()
@@ -106,17 +110,40 @@ class Stroke_canvas(Canvas):
         submit_bttn = ttk.Button(message_window, text="Submit", command=store_stroke).grid(row=2, column=0)
         cancel_bttn = ttk.Button(message_window, text="Cancel", command=lambda: close_window(message_window)).grid(row=2, column=1)
 
+    def set_current_letter(self, in_letter):
+        current_letter_var.set(in_letter)
+        self.letter_loaded = True
+        self.show_outline()
+
     def load_strokes(self):
         # choose_window = Toplevel(root)
         # choose_bttn = ttk.Button(choose_window, text="A").grid(row=0, column=0)
-        
-        self.letter_loaded = True
-        self.show_outline()
+        self.show_load_options()
+
         # if (exists(self.csvfile_name)):
         #     pass
         # else:
         #     close_window(choose_window)
 
+    def show_load_options(self):
+        choose_window = Toplevel(root)
+        col_ct = 0
+        row_ct = 0
+        button_ids = []
+        with open(self.csvfile_name, 'r', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                button_letter = row['symbol_name']
+                button_id = ttk.Button(choose_window, text=button_letter, command = lambda text=button_letter:self.set_current_letter(text))
+                button_ids.append(button_id)
+
+            for bid in button_ids:
+                bid.grid(column=col_ct, row=row_ct)
+                col_ct +=1
+                if col_ct > 4:
+                    col_ct = 0
+                    row_ct += 1
+                
     def show_outline(self):
         if self.letter_loaded:
             clear_canvas()
@@ -124,7 +151,8 @@ class Stroke_canvas(Canvas):
                 with open(self.csvfile_name, 'r', newline='') as csvfile:
                     reader = csv.DictReader(csvfile)
                     for row in reader:
-                        if row['symbol_name'] == "h":
+                        if row['symbol_name'] == current_letter_var.get():
+                            hint_lbl['text'] = current_letter_var.get()
                             for stroke in row['strokes'].split('-'):
                                 if show_arrows_var.get():
                                     self.create_line(*stroke.split(';'), 
@@ -132,6 +160,7 @@ class Stroke_canvas(Canvas):
                                 else:
                                     self.create_line(*stroke.split(';'), 
                                         width=3, fill="grey")
+                    
 
 
     def change_pt_color_to_red(self, event):
@@ -151,7 +180,7 @@ class Stroke_canvas(Canvas):
 
 content = ttk.Frame(root, padding=(3,3,12,12))
 name_lbl = ttk.Label(content, text="Prompt: ")
-hint_lbl = ttk.Label(content, font=("Arial", 50), text="test")
+hint_lbl = ttk.Label(content, font=("Arial", 50))
 stroke_num_lbl = ttk.Label(content, text="Stroke: ")
 
 sketch = Stroke_canvas(content, language="EN", height=100, width=400)
@@ -167,6 +196,7 @@ def clear_canvas():
     stroke_number_var.set(0)
     stroke_num_lbl.config(text="Stroke: ")
     sketch.clear_stroke()
+    hint_lbl['text'] = ''
 
 show_outline_bttn = ttk.Checkbutton(content, text="Outline", 
     variable=show_outline_var, onvalue=True, command=sketch.show_outline)
